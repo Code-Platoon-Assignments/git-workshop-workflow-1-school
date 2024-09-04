a3daa5e7ee47f68e72d33bbfbd59313f0726e5dd
mikhail <mikhail.waddell@gmail.com>
2024-09-04 11:18:12 -0400

    changed student to person

--- backend/app.py
+++ backend/app.py
@@ -1,48 +1,76 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_cors import CORS
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.environ['FLASK_ENV'])
-print(os.environ['MY_SECRET_API_KEY'])
+print(os.environ["FLASK_ENV"])
+print(os.environ["MY_SECRET_API_KEY"])
 
 app = Flask(__name__)
 CORS(app)
 
 
-class Student:
-    """student"""
+class Person:
+    """Person"""
+
     def __init__(self, data):
-        self.id = data.get('id')
-        self.first_name = data.get('first_name')
-        self.last_name = data.get('last_name')
+        self.id = data.get("id")
+        self.first_name = data.get("first_name")
+        self.last_name = data.get("last_name")
 
-students_list = []
 
-# Add some students
-students_list.append(Student({'id': 1, 'first_name': 'Han', 'last_name': 'Solo', 'email' : 'han@email.com'}))
-students_list.append(Student({'id': 2, 'first_name': 'Leia', 'last_name': 'Skywalker', 'email' : 'leia@gmail.com'}))
-students_list.append(Student({'id': 3, 'first_name': 'Luke', 'last_name': 'Skywalker', 'email' : 'luke@email.com'}))
+person_list = []
 
-@app.route('/', methods=['GET'])
+# Add some person
+person_list.append(
+    Person(
+        {"id": 1, "first_name": "Han", "last_name": "Solo", "email": "han@email.com"}
+    )
+)
+person_list.append(
+    Person(
+        {
+            "id": 2,
+            "first_name": "Leia",
+            "last_name": "Skywalker",
+            "email": "leia@gmail.com",
+        }
+    )
+)
+person_list.append(
+    Person(
+        {
+            "id": 3,
+            "first_name": "Luke",
+            "last_name": "Skywalker",
+            "email": "luke@email.com",
+        }
+    )
+)
+
+
+@app.route("/", methods=["GET"])
 def base_route():
     return "hello"
 
-@app.route('/students', methods=['GET'])
-def get_students():
+
+@app.route("/person", methods=["GET"])
+def get_person():
     print("Adam's print statement on line 34.")
-    """get all students"""
-    student_list = [
-        {'id': student.id, 'first_name': student.first_name, 'email': student.email}
-        for student in students_list
+    """get all person"""
+    Person_list = [
+        {"id": Person.id, "first_name": Person.first_name, "email": Person.email}
+        for Person in person_list
     ]
 
-    return jsonify(student_list)
+    return jsonify(Person_list)
+
 
 IS_DEBUG_ENABLED = False
-if os.environ['FLASK_ENV'] == 'dev':
+if os.environ["FLASK_ENV"] == "dev":
     IS_DEBUG_ENABLED = True
 
 app.run(debug=IS_DEBUG_ENABLED)
